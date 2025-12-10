e7c700f46ea9d59612b37b6a32dcb670d7bcc461
Ashley Sands <ashley.sands01@gmail.com>
2025-12-10 10:05:28 -0800

    add show page

--- controllers/applications.js
+++ controllers/applications.js
@@ -41,4 +41,23 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/:applicationId', async (req, res) => {
+    try {
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id)
+        // Find the application by the applicationId supplied from req.params
+        const application = currentUser.applications.id(
+            req.params.applicationId
+        )
+        // Render the show view, passing the application data in the context object
+        res.render('applications/show.ejs', {
+            application: application,
+        })
+    } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
 module.exports = router
