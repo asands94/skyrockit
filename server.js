1ac309b78622e374c7da7f873d60f795250ce95a
Ashley Sands <ashley.sands01@gmail.com>
2025-12-10 10:29:40 -0800

    add commnts to server.js

--- server.js
+++ server.js
@@ -1,52 +1,62 @@
+// import installed packages
 const dotenv = require('dotenv')
-dotenv.config()
+dotenv.config() // read variables from .env file
+
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const morgan = require('morgan')
 const session = require('express-session')
 
-const isSignedIn = require('./middleware/is-signed-in.js')
-const passUserToView = require('./middleware/pass-user-to-view.js')
+// import custom middleware
+const isSignedIn = require('./middleware/is-signed-in.js') // checks if user is logged in
+const passUserToView = require('./middleware/pass-user-to-view.js') // makes user available in views
 
-const authController = require('./controllers/auth.js')
-const applicationsController = require('./controllers/applications.js')
+// import routes
+const authController = require('./controllers/auth.js') // routes for authentication
+const applicationsController = require('./controllers/applications.js') // routes for applications
 
-const port = process.env.PORT ? process.env.PORT : '3000'
+const app = express() // create an express app
+const port = process.env.PORT ? process.env.PORT : '3000' // choose port from env or default to 3000
 
+// database connection
 mongoose.connect(process.env.MONGODB_URI)
-
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
 })
 
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
-app.use(morgan('dev'))
+// Use the middleware that was imported
+app.use(express.urlencoded({ extended: false })) // parse form data
+app.use(methodOverride('_method')) // allow PUT/DELETE requests
+app.use(morgan('dev')) // console log incoming requests
 
 app.use(
     session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
+        secret: process.env.SESSION_SECRET, // secret for signing session ID
+        resave: false, // don't resave unchanged sessions
+        saveUninitialized: true, // save new sessions
     })
 )
 
-app.use(passUserToView)
+app.use(passUserToView) // pass the user information to all views
 
+// routes
 app.get('/', (req, res) => {
+    // homepage route
     if (req.session.user) {
-        res.redirect(`/users/${req.session.user._id}/applications`)
+        // if user is logged in
+        res.redirect(`/users/${req.session.user._id}/applications`) // send them to their apps page
     } else {
-        res.render('index.ejs')
+        res.render('index.ejs') // otherwise show landing page
     }
 })
 
-app.use('/auth', authController)
-app.use(isSignedIn)
-app.use('/users/:userId/applications', applicationsController)
+app.use('/auth', authController) // use auth routes
+app.use(isSignedIn) // routes below this line require a user to be signed in
 
+app.use('/users/:userId/applications', applicationsController) // use applications routes
+
+// start the server
 app.listen(port, () => {
     console.log(`The express app is ready on port ${port}!`)
 })
